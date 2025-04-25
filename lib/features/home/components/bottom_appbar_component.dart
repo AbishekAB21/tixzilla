555574ad2cdb44d278c9d284248e7af471c72e1c
abishek <abishek.ab@ust.com>
2025-04-25 23:37:50 +0530

    bottom nav bar changes

--- lib/features/home/components/bottom_appbar_component.dart
+++ lib/features/home/components/bottom_appbar_component.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:tixzilla/utils/themes/themes.dart';
 
 class BottomAppbarComponent extends StatelessWidget {
@@ -9,6 +10,8 @@ class BottomAppbarComponent extends StatelessWidget {
     return  Scaffold(
       backgroundColor: appcolor.background,
       body: Center(child: Text("data"),),
+
+      // Add a bottom nav bar here Movies-Events-My Bookings-Profile
     );
   }
 }
