b3a0717f1d05e7cb6533d6f2d1ec76af1f9bb463
AbishekAB21 <abishekab1805@outlook.com>
2025-04-28 23:30:08 +0530

    overflow fix

--- lib/features/movies/components/movie_screen_component.dart
+++ lib/features/movies/components/movie_screen_component.dart
@@ -58,70 +58,73 @@ class MovieScreenComponent extends ConsumerWidget {
       body: Center(
         child: Padding(
           padding: EdgeInsets.all(10),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 550,
-                child: PageView.builder(
-                  controller: pageController,
-                  onPageChanged: (index) {
-                    ref.read(currentPageProvider.notifier).state = index;
-                  },
-                  itemCount: movies.length,
-                  itemBuilder: (context, index) {
-                    final movie = movies[index];
-                    return Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          // Scrollable poster widget
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                CustomFadeTransition(
-                                  route: MovieDetailsContainer(),
-                                ),
-                              );
-                            },
-                            child: PosterWidget(
-                              posterPath: movie["poster"] as String,
+          child: SingleChildScrollView(
+            physics: NeverScrollableScrollPhysics(),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 550,
+                  child: PageView.builder(
+                    controller: pageController,
+                    onPageChanged: (index) {
+                      ref.read(currentPageProvider.notifier).state = index;
+                    },
+                    itemCount: movies.length,
+                    itemBuilder: (context, index) {
+                      final movie = movies[index];
+                      return Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            // Scrollable poster widget
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  CustomFadeTransition(
+                                    route: MovieDetailsContainer(),
+                                  ),
+                                );
+                              },
+                              child: PosterWidget(
+                                posterPath: movie["poster"] as String,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
-              SizedBox(height: 30),
-              Text(
-                movies[currentPage]["title"] as String,
-                style: Fontstyles.bebasNeue25px(context),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
-              // Ratings System
-              RatingBar.builder(
-                initialRating: movies[currentPage]["rating"] as double,
-                itemCount: 5,
-                allowHalfRating: true,
-                unratedColor: appcolor.iconColor,
-                itemSize: 25.0,
-                ignoreGestures: true,
-                itemPadding: EdgeInsets.symmetric(horizontal: 2),
-                itemBuilder:
-                    (context, index) => Image.asset("assets/images/star.png"),
-                onRatingUpdate: (value) {},
-              ),
-              SizedBox(height: 10),
-              // Review
-              Text(
-                movies[currentPage]["review"] as String,
-                style: Fontstyles.inter600w(context),
-              ),
-            ],
+                SizedBox(height: 30),
+                Text(
+                  movies[currentPage]["title"] as String,
+                  style: Fontstyles.bebasNeue25px(context),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                // Ratings System
+                RatingBar.builder(
+                  initialRating: movies[currentPage]["rating"] as double,
+                  itemCount: 5,
+                  allowHalfRating: true,
+                  unratedColor: appcolor.iconColor,
+                  itemSize: 25.0,
+                  ignoreGestures: true,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 2),
+                  itemBuilder:
+                      (context, index) => Image.asset("assets/images/star.png"),
+                  onRatingUpdate: (value) {},
+                ),
+                SizedBox(height: 10),
+                // Review
+                Text(
+                  movies[currentPage]["review"] as String,
+                  style: Fontstyles.inter600w(context),
+                ),
+              ],
+            ),
           ),
         ),
       ),
